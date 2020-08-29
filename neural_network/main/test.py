d28714d2abd513780a3733a4e1d76d4181172445
RandomUserIK <xkovaci1@stuba.sk>
2020-08-29 20:42:24 +0200

    feat(neural_network):
    - add .json files for train and validation sets
    - add Mask R-CNN files
    which work with Tensorflow >= 2.0.0
    - add script to test the trained
    model on a single image

---
+++ neural_network/main/test.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+import utils
+import mrcnn
+from mrcnn.visualize import display_images
+import mrcnn.model as modellib
+from mrcnn.model import log
+
+import roi
+
+# Directory to save logs and trained model
+MODEL_DIR = ROOT_DIR
+
+ROI_WEIGHTS_PATH = "./mask_rcnn_roi.h5"  # TODO: update this path
+
+config = roi.RoiConfig()
+ROI_DIR = os.path.join(ROOT_DIR, "dataset")
+
+# Override the training configurations with a few
+# changes for inferencing.
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+# Device to load the neural network on.
+# Useful if you're training a model on the same 
+# machine, in which case use CPU and leave the
+# GPU for training.
+DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0
+
+# Inspect the model in training or inference modes
+# values: 'inference' or 'training'
+TEST_MODE = "inference"
+
+
+def get_ax(rows=1, cols=1, size=16):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Adjust the size attribute to control how big to render images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+# Load validation dataset
+dataset = roi.RoiDataset()
+dataset.load_roi(ROI_DIR, "val")
+
+# Must call before using the dataset
+dataset.prepare()
+
+print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
+
+# Create model in inference mode
+with tf.device(DEVICE):
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR,
+                              config=config)
+
+# Set path to balloon weights file
+
+# Or, load the last model you trained
+weights_path = ROI_WEIGHTS_PATH
+
+# Load weights
+print("Loading weights ", weights_path)
+model.load_weights(weights_path, by_name=True)
+
+image_id = dataset.image_ids[1]
+image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, image_id)
+info = dataset.image_info[image_id]
+print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id, 
+                                       dataset.image_reference(image_id)))
+
+# Run object detection
+results = model.detect([image], verbose=1)
+
+# Display results
+ax = get_ax(1)
+r = results[0]
+mrcnn.visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            dataset.class_names, r['scores'], ax=ax,
+                            title="Predictions")
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+log("gt_mask", gt_mask)
