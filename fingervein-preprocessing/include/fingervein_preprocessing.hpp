8b960b063199077438eafbbaf7da9a3f2edec0fc
xkovac <xkovaci1@stuba.sk>
2020-11-22 00:44:10 +0100

    feature(utility): implement utility conversion methods

--- fingervein-preprocessing/include/fingervein_preprocessing.hpp
+++ fingervein-preprocessing/include/fingervein_preprocessing.hpp
@@ -1,6 +1,25 @@
 #ifndef OPENFINGER_FINGERVEIN_PREPROCESSING_HPP
 #define OPENFINGER_FINGERVEIN_PREPROCESSING_HPP
 
+// std
+#include <vector>
+#include <memory>
+
+// ArrayFire
+#include <arrayfire.h>
+
+// OpenCV
+#include <opencv4/opencv2/opencv.hpp>
+
+// Qt
+#include <QDebug>
+#include <QString>
+#include <QImage>
+
+// Custom
 #include "preprocessing/preprocessing.hpp"
 
 #endif //OPENFINGER_FINGERVEIN_PREPROCESSING_HPP
+
+
+
---
+++ fingervein-preprocessing/include/utility/utility.hpp
@@ -0,0 +1,71 @@
+#ifndef OPENFINGER_UTILITY_HPP
+#define OPENFINGER_UTILITY_HPP
+
+#include "fingervein_preprocessing.hpp"
+
+namespace utility {
+
+    static inline QImage mat2qImage(const cv::Mat &inputMat, const QImage::Format format) {
+        return QImage(inputMat.data, inputMat.cols, inputMat.rows, static_cast<int>(inputMat.step), format);
+    }
+
+    static inline cv::Mat qImage2Mat(const QImage &inputQImage, const int format) {
+        return cv::Mat(inputQImage.height(), inputQImage.width(), format, const_cast<uchar*>(inputQImage.bits()),
+                       static_cast<size_t>(inputQImage.bytesPerLine()));
+    }
+
+    static inline af::array mat_uchar2array_uchar(const cv::Mat &inputMat) {
+        if (inputMat.type() != CV_8UC1) {
+            // TODO: replace QDebug with a logging library (e.g. easyLogging++)
+            qDebug() << QString("OpenCV Mat to AF Array: input image is not grayscale.\n");
+            throw std::invalid_argument("Input cv::Mat is of invalid type. Type required: CV_8UC1");
+        }
+        cv::Mat transposed;
+        cv::transpose(inputMat, transposed);
+        return af::array(inputMat.rows, inputMat.cols, transposed.data);
+    }
+
+    static inline cv::Mat array_uchar2mat_uchar(const af::array &inputArray) {
+        if (inputArray.type() != u8) {
+            // TODO: replace QDebug with a logging library (e.g. easyLogging++)
+            qDebug() << QString("af::array to cv::Mat: input image is not grayscale.\n");
+            throw std::invalid_argument("Input af::array is of invalid type. Type required: u8");
+        }
+        auto *afData = inputArray.as(u8).T().host<uchar>();
+        cv::Mat outputMat = cv::Mat(static_cast<int>(inputArray.dims(0)),
+                                    static_cast<int>(inputArray.dims(1)),
+                                    CV_8UC1, afData);
+        af::freeHost(afData);
+        return outputMat;
+    }
+
+    static inline af::array mat_double2array_double(const cv::Mat &inputMat) {
+        // TODO: is an additional check for double type really necessary?
+        if (inputMat.type() != CV_64F) {
+            // TODO: replace QDebug with a logging library (easyLogging++)
+            qDebug() << QString("OpenCV Mat to AF Array: input image is not grayscale.\n");
+            throw std::invalid_argument("Input cv::Mat is of invalid type. Type required: CV_64F");
+        }
+
+        std::vector<double> imgData(static_cast<unsigned long>(inputMat.rows * inputMat.cols));
+        for (int col = 0; inputMat.cols; ++col) {
+            for (int row = 0; inputMat.rows; ++row) {
+                imgData.emplace_back(inputMat.at<double>(row, col));
+            }
+        }
+        return af::array(inputMat.rows, inputMat.cols, imgData.data());
+    }
+
+    static inline void af_normalizeImage(af::array &inputArray) noexcept {
+        af::array min = af::tile(af::min(inputArray),
+                                 static_cast<unsigned int>(inputArray.dims(0)),
+                                 static_cast<unsigned int>(inputArray.dims(1)));
+        af::array max = af::tile(af::max(inputArray),
+                                 static_cast<unsigned int>(inputArray.dims(0)),
+                                 static_cast<unsigned int>(inputArray.dims(1)));
+        inputArray = 255 * ((inputArray.as(f32) - min) / (max - min));
+    }
+
+}
+
+#endif //OPENFINGER_UTILITY_HPP
